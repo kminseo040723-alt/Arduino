acd845ca55e9b7c2dddd17dca515cba4d0a5c10c
kminseo040723@gmail.com <kminseo040723@gmail.com>
2026-06-01 17:30:30 +0900

    2026/06/01

--- 코드/gear_motor/gear_motor.ino
+++ 코드/gear_motor/gear_motor.ino
@@ -1,15 +1,12 @@
 /////////////////
 ///TBTB6612FNG///
 /////////////////
-const int STBY = 8;
+const int STBY = 9;
 //MOTOT A
 const int PWMA = 5;
-const int AIN1 = 7;
-const int AIN2 = 6;
-//MOTOT B
-const int PWMB = 11;
-const int BIN1 = 9;
-const int BIN2 = 10;
+const int AIN1 = 3;
+const int AIN2 = 4;
+
 void setup() {
   pinMode(STBY, OUTPUT);
 
@@ -17,32 +14,16 @@ void setup() {
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
 
-  pinMode(PWMB, OUTPUT);
-  pinMode(BIN1, OUTPUT);
-  pinMode(BIN2, OUTPUT);
 
   digitalWrite(STBY, HIGH);
 
 }
 
 void loop() {
-   digitalWrite(BIN1, HIGH);
-  digitalWrite(BIN2, LOW);
-  analogWrite(PWMB, 255);
+   digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(PWMA, 255);
 
   delay(3000);
 
-  // 정지
-  analogWrite(PWMB, 0);
-  delay(1000);
-
-  // 역방향
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, HIGH);
-  analogWrite(PWMB, 255);
-
-  delay(3000);
-
-  analogWrite(PWMB, 0);
-  delay(1000);
 }
